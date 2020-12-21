631b271c4f0e33ec3529c4461a2058e05fb906bd
creotip <creotip@gmail.com>
2020-12-21 03:24:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import {
+  ChakraProvider,
+  CSSReset,
+  Box,
+  Button,
+  SimpleGrid,
+  Flex,
+  Input,
+} from '@chakra-ui/react'
+import useFetch from 'use-http'
+import TransactionsList from './TransactionsList'
+import isSolid from 'is-solid'
+
+const API_BASE_URL = 'https://api.etherscan.io/'
+const API_KEY = 'BFK2C4SZU96PAIV7N1TVNJJ8TQFP6B79DV'
+const DEFAULT_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 
 function App() {
+  const [urlToFetch, setUrlToFetch] = useState(null)
+  const [etheriumAddress, setEtheriumAddress] = useState(DEFAULT_ADDRESS)
+  const [transactions, setTransactions] = useState(null)
+  const { get, response, loading, error } = useFetch(API_BASE_URL)
+
+  useEffect(() => {
+    createUrlToFetch()
+  }, [etheriumAddress])
+
+  const createUrlToFetch = (address = etheriumAddress) => {
+    const url = `api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${API_KEY}`
+
+    setUrlToFetch(url)
+  }
+
+  const getTransactions = async () => {
+    const response = await get(urlToFetch)
+
+    if (response.message === 'OK') {
+      setTransactions(response.result)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ChakraProvider>
+      <CSSReset />
+      <Box p="2rem" maxWidth="1200px" mx="auto" height="100%">
+        {/* <HookForm /> */}
+        <Flex mb="1rem" pb="1rem" borderBottom="1px solid #e7eaf3">
+          <Box width={[1 / 2]}>
+            <Input
+              value={etheriumAddress}
+              placeholder="Add etherium address"
+              onChange={(e) => setEtheriumAddress(e.target.value)}
+            />
+          </Box>
+          <Button onClick={getTransactions} isLoading={loading} mx="1rem">
+            Fetch Transactions
+          </Button>
+        </Flex>
+
+        <SimpleGrid
+          minChildWidth="120px"
+          columns={5}
+          spacing={10}
+          pr="1rem"
+          pb="1rem"
+          mb="1rem"
+          borderBottom="1px solid #e7eaf3"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Box>#</Box>
+          <Box>timeStamp</Box>
+          <Box>from</Box>
+          <Box>to</Box>
+          <Box>value</Box>
+          <Box>confirmations</Box>
+        </SimpleGrid>
+        {isSolid(transactions) && (
+          <TransactionsList transactions={transactions} />
+        )}
+      </Box>
+    </ChakraProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/TransactionsList.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { FixedSizeList as List } from 'react-window'
+import { Flex, Tooltip, Box, SimpleGrid, useClipboard } from '@chakra-ui/react'
+import AutoSizer from 'react-virtualized-auto-sizer'
+import { CopyIcon } from '@chakra-ui/icons'
+import dayjs from 'dayjs'
+
+const Row = ({ data, index, style }) => {
+  const { timeStamp, from, to, value, confirmations } = data[index]
+
+  return (
+    <SimpleGrid
+      style={style}
+      fontSize="13px"
+      minChildWidth="120px"
+      columns={5}
+      spacing={10}
+    >
+      <Box>{index + 1}</Box>
+      <Box whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
+        {timeStamp}
+      </Box>
+      <Flex>
+        <Box whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
+          <Tooltip
+            hasArrow
+            placement="top-start"
+            label={from}
+            aria-label="A tooltip"
+          >
+            {from}
+          </Tooltip>
+        </Box>
+        <CopyIcon cursor="pointer" />
+      </Flex>
+      <Flex>
+        <Box whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
+          <Tooltip
+            hasArrow
+            placement="top-start"
+            label={to}
+            aria-label="A tooltip"
+          >
+            {to}
+          </Tooltip>
+        </Box>
+        <CopyIcon cursor="pointer" />
+      </Flex>
+      <Box whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
+        {value}
+      </Box>
+      <Box whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
+        {confirmations}
+      </Box>
+    </SimpleGrid>
+  )
+}
+
+const TransactionsList = ({ transactions }) => {
+  return (
+    <AutoSizer>
+      {({ height, width }) => (
+        <List
+          className="List"
+          height={height}
+          itemData={transactions}
+          itemCount={transactions.length}
+          itemSize={35}
+          width={width}
+        >
+          {Row}
+        </List>
+      )}
+    </AutoSizer>
+  )
+}
+
+export default TransactionsList
